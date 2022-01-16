662d073aec3f68b4827eba2a3ce47216c4acd98b
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-01-16 15:31:46 +0530

    Route for Create User

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,35 +1,44 @@
 const axios = require("axios");
 
 exports.signup = async (req, res) => {
-  // const { email, password, firstName, lastName } = req.body;
-
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.username
+  ) {
+    res.status(400).json({ message: "Bad Request" });
+  }
   var data = JSON.stringify({
-    name: "Yash Shivhare",
-    email: "vs.doce@gmail.com",
-    password: "shivhare10shivhare10",
-    username: "shivhare10",
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username,
     active: true,
     approved: true,
     "user_fields[1]": "sed dolor",
   });
   var config = {
     method: "post",
-    url: "{{baseUrl}}/users.json",
+    url: `${process.env.DISCOURSE_URL}/users.json`,
     headers: {
-      "Api-Key": "{{apikey}}",
-      "Api-Username": "{{apiusername}}",
+      "Api-Key": process.env.DISCOURSE_API_KEY,
+      "Api-Username": process.env.DISCOURSE_ADMIN_USERNAME,
       "Content-Type": "application/json",
     },
     data: data,
   };
+  console.log(config);
   try {
     axios(config)
       .then(function (response) {
-        res.status(201).json({ data: "Success" });
+        res.status(201).json({ data: response.data });
       })
       .catch(function (error) {
+        res.status(500).json({ message: "Something went wrong" });
         console.log(error);
       });
+    // res.status(201).json({ data: "Success" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
