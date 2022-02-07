fec515f150911933b44295b4eba6f706a1d024da
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-02-07 20:25:12 +0530

    Send Waiting Email

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const TopicCron = require("./CronSchedule/discourseTopic");
+const SendReminder = require("./CronSchedule/SendReminer");
 dotenv.config();
 // Starting the Server
 const port = process.env.PORT || 8000;
@@ -8,3 +9,4 @@ const server = app.listen(port, () => {
   console.log(`App running on Port ${port}...`);
 });
 // TopicCron();
+// SendReminder();
